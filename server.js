b42c44eddfb65ac0ca8436c79d081718a37d9d16
anelson92 <115906480+anelson92@users.noreply.github.com>
2023-02-05 22:28:01 -0600

    Add files via upload

---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express')
+const fs = require('fs')
+const path = require('path')
+const notes = require('./db/db.json');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const PORT = 3001; 
+
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static('public'));
+
+const readAndAppend = (content) => {
+    fs.readFile('db.json', (err, data) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      const parsedDataArray = JSON.parse(data);
+      parsedDataArray.push(content);
+      content.id = uuidv4();
+      writeToFile('db.json', JSON.stringify(parsedDataArray));
+      // maybe have this function return the updated notes
+      return parsedDataArray;
+    }
+  })
+};
+
+const writeToFile = (destination, content) =>
+  fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+    err ? console.error(err) : console.info(`\nData written to ${destination}`)
+  );
+
+  
+app.get('/api/notes', (req, res) => res.json(notes))
+
+// HTML routes
+// GET route to retieve notes.html
+app.get('/public/notes.html',function(req, res) {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+});
+
+// GET /api/notes to read the db.json and return all saved notes as JSON
+// app.get('/api/notes/', (req, res) => {
+//   res.json(notes)
+// })
+
+// POST /api/notes to save new notes on request body and save it to the JSON file "You'll need to find a way to give each note a unique id when it's saved"
+app.post('/api/notes/', (req, res) => {
+  const newNote = {
+    title: req.body.title,
+    text: req.body.text,
+    id: uuidv4()
+  };
+
+  const updatedNotesArray = readAndAppend(newNote);
+  res.status(200).json( { updatedNotes: updatedNotesArray })
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  notes.forEach((note, index) => {
+    if(req.params.id === note.id) {
+      notes.splice(index, 1)
+    }
+  })
+
+  writeToFile('db/db.json', notes)
+})
+
+// GET * route to retrieve the index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'));
+});
+
+
+app.listen(PORT, () => 
+  console.log(`Note taking app listening on ${PORT}!`)
+)
